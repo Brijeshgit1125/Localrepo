7ecdb84a75c71184470cdb8bec935f7eeb23c732
Brijeshgit1125 <brijeshkulkarni1125@gmail.com>
2025-07-27 13:46:16 +0530

    Feature1 branch changes. Should be visible only in feature1

--- hello.py
+++ hello.py
@@ -1,1 +1,3 @@
 print("Git is a bit tricky to learn at first, but it's very powerful.")
+print("I am trying this in feature1 branch. It should appear only in this branch.")
+print("It should not appear in main branch.")
